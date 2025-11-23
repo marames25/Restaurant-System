01f6cbad92985f780d54296e3061dc3596f73a68
Yusif <yk2385501@gmail.com>
2025-11-23 21:40:41 +0200

    removed unused walledId

--- src/restaurant/payment/methods/MobileWallet.java
+++ src/restaurant/payment/methods/MobileWallet.java
@@ -2,13 +2,6 @@ package restaurant.payment.methods;
 
 public class MobileWallet implements PaymentMethod {
 
-    private String walletID;
-
-    public MobileWallet(String walletID) {
-        this.walletID = walletID;
-
-    }
-
     @Override
     public void pay(double amount) {
         System.out.println("Paying " + amount + " for mobile wallet");
