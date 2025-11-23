35d3e29bade269cbd5891738f7948cee3d97f8a4
Yusif <yk2385501@gmail.com>
2025-11-23 12:40:24 +0200

    added menu and its subtypes

--- src/restaurant/menu/component/Menu.java
+++ src/restaurant/menu/component/Menu.java
@@ -1,11 +1,36 @@
 package restaurant.menu.component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Menu extends MenuComponent {
 
     protected List<MenuComponent> components;
+    protected String type;
+
+    public Menu(String type) {
+        this.type = type;
+        components = new ArrayList<>();
+    }
 
     @Override
-    public String display() { return null; }
+    public void display() { 
+        System.out.println(type + " menu: ");
+
+        for (MenuComponent mc : components) {
+            mc.display();
+        }
+    }
+
+    @Override
+    public void addComponent(MenuComponent component) {
+        components.add(component);
+    }
+
+    @Override
+    public void removeComponent(MenuComponent component) {
+        components.remove(component);
+    }
+
+
 }
